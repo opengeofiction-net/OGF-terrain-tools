c19abd763c46d5796722130c489146b319dbf8c3
Lee Kindness <lee@leekindness.com>
2025-06-25 14:26:43 +0100

    reset

--- bin/overpassCacheAirports.pl
+++ bin/overpassCacheAirports.pl
@@ -275,6 +275,11 @@ for my $record ( @$records )
 			$entry->{'terminals:count'}++;
 			push @{$entry->{'terminals'}}, $name;
 		}
+		elsif( $entry->{'terminals:count'} == 0 )
+		{
+			# in the 1 terminal scenario, it's possible it is unnamed...
+			$entry->{'terminals:count'} = 1;
+		}
 	}
 	else
 	{
